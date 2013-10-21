5ba771614d367378842044c703fd45cb64940995
EpicPhill <pr6g13@ghoul.ecs.soton.ac.uk>
2013-10-21 14:33:39 +0100

    removed Color component

--- SpirographPoint.java
+++ SpirographPoint.java
@@ -3,7 +3,6 @@ import java.awt.*;
 public class SpirographPoint{
 
 	protected double x, y;
-	protected Color colour;
 	
 	public SpirographPoint(double xCoord, double yCoord){
 		x = xCoord;
@@ -16,10 +15,6 @@ public class SpirographPoint{
 	
 	public double getYCoord(){
 		return y;
-	}
-	
-	public Color getColor(){
-		return colour;
 	}
 	
 }
